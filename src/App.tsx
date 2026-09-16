47b7a0ebec6af793063db123d064172d21268a3f
1/3 NT <you@example.com>
2026-09-16 13:21:05 +0200

    props drilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState } from "react"
+import ButtonWrapper from "./components/ButtonWrapper"
+import Screen from "./components/Screen"
+import ThreeStateSwitch from "./components/ThreeStateSwitch"
+
+export type ModeSwitchType = {
+  mode: number,
+  nextMode: () => void
+}
 
 const App = () => {
+  const [mode, setMode] = useState(0)
+
+  const nextMode = () => {
+    // 0 => 1%3 = 1
+    // 1 => 2%3 = 2
+    // 2 => 3%3 = 0
+    // 0 => 1%3 = 1
+    setMode(prev => (prev + 1) % 3)
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <ThreeStateSwitch {...{mode, nextMode}}/>
+      <Screen {...{mode, nextMode}} />
+      <ButtonWrapper mode={mode} nextMode={nextMode} />
+    </div>
   )
 }
 
